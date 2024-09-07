aa4158802db75b2cd6571b8e46314c28159eeda1
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-09-07 14:20:25 -0700

    Add files via upload
    done with Master Setting Field Values in JavaScript!

---
+++ index.js
@@ -0,0 +1,23 @@
+// function setAreaName() {
+//     var zipCode = +document.getElementById("zipcode").value;
+//     var area = document.getElementById("area");
+
+    // we've chosen different areas in Islamabad
+    // 45200 -> Golra
+    // 44210 -> Islamabad f-7 markaz
+    // 44320 -> Islamabad fedral board
+    // 44310 -> Islamabad Allama iqbal open university
+    // * -> Invalid zipcode
+
+//     if (zipCode === 45200) {
+//         area.value = "Golra"
+//     } else if (zipCode === 44210) {
+//         area.value = "Islamabad f-7 markaz"
+//     } else if (zipCode === 44320) {
+//         area.value = "Islamabad fedral board"
+//     } else if (zipCode === 44310) {
+//         area.value = "Islamabad Allama iqbal open university"
+//     } else {
+//         alert("Invalid zipcode");
+//     }
+// }
